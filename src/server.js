63a6c8204e0f25695c61d6f57e49689c8630d239
dennismuturi <md.dennismuturi@gmail.com>
2026-02-08 21:43:07 +0200

    created routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express()
 const PORT = 5001;
+
+
+app.get("/hello", (req,res)=> {
+    res.json({message: "Hello World"})
+})
+
 const server = app.listen(PORT, () => {
     console.log(`Server is runnig on PORT ${PORT}`)
 })
